1cb1a1ad04d68c33f96af55823c66f82808553d9
Kyle Chung <kylechun9@gmail.com>
2015-11-09 21:49:01 +0800

    complete prototype

--- match_crawlerd.py
+++ match_crawlerd.py
@@ -22,30 +22,50 @@ def setRedisConn(host="localhost", port="6379"):
     r = redis.Redis(host=host, port=port, db=0)
     return r
 
+def increaseId(match_id, inc=1):
+    next_match_id = str(int(match_id) + inc)
+    return next_match_id
+
 def main():
-    while True:
-        match_details = getMatchDetails(region=region, match_id=match_id, api_key=api_key)
-        if match_details is not None:
-            mid = match_details.pop("matchId")
-            redis.hmset(mid, match_details)
-        time.sleep(1)
+    config = ConfigParser.ConfigParser()
+    if len(config.read(['conf.ini'])):
+        api_key = config.get("user", "api_key")
+        redis_host = config.get("redis", "host")
+        redis_port = config.get("redis", "port")
+        redis = setRedisConn(host=redis_host, port=redis_port)
+        last_match = redis.lindex("last100", 0)
+        if last_match is None: # for brand-new database
+            match_id = "2083727714"
+        else:
+            match_id = increaseId(last_match)
+        region = "kr"
+
+        while True:
+            # crawl the match data
+            match_details = getMatchDetails(region=region, match_id=match_id, api_key=api_key)
+
+            if match_details is not None:
+
+                # insert match data into redis
+                mid = match_details.pop("matchId")
+                redis.hmset(mid, match_details)
+                
+                # update list of last-inserted
+                redis.lpush("last100", mid)
+                redis.ltrim("last100", 0, 99)
+
+            match_id = increaseId(match_id)    
+            time.sleep(1) # to avoid api overshooting (max 500 queries per 10 min)
+    else:
+        print "File conf.ini not found. Program aborted."
+        exit(1)
 
 def runAsDaemon():
     with daemon.DaemonContext():
         main()
 
 if __name__ == "__main__":
-    config = ConfigParser.ConfigParser()
-    if len(config.read(['conf.ini'])):
-        api_key = config.get("user", "api_key")
-        region = "kr"
-        redis_host = config.get("redis", "host")
-        redis_port = config.get("redis", "port")
-        redis = setRedisConn(host=redis_host, port=redis_port)
-        runAsDaemon()
-    else:
-        print "File conf.ini not found. Program aborted."
-        exit(1)
+    runAsDaemon()
 
 
 
