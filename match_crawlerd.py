b042fcc6db54d61a806e0463e6778791e0592ba2
Kyle Chung <kylechun9@gmail.com>
2015-11-10 13:48:46 +0800

    use last match id of seed player for brand-new db case

--- match_crawlerd.py
+++ match_crawlerd.py
@@ -7,6 +7,18 @@ import daemon
 import redis
 import time
 
+def getLastMatch(region, seed_pid, api_key, ver="v1.3"):
+    api_server = "https://%s.api.pvp.net" % region
+    api_call = "%s/api/lol/%s/%s/game/by-summoner/%s/recent?" % (api_server, region, ver, seed_pid)
+    qs = {"api_key": api_key}
+    r = requests.get(api_call, params=qs)
+    if r.status_code == 200:
+        rj = r.json()
+        last_match = rj["games"][0]["gameId"]
+        return last_match
+    else:
+        return None
+
 def getMatchDetails(region, match_id, api_key, ver="v2.2"):
     api_server = "https://%s.api.pvp.net" % region
     api_call = "%s/api/lol/%s/%s/match/%s" % (api_server, region, ver, match_id)
@@ -36,7 +48,7 @@ def main():
         redis = setRedisConn(host=redis_host, port=redis_port)
         last_match = redis.lindex("last100", 0)
         if last_match is None: # for brand-new database
-            match_id = "2083727714"
+            match_id = getLastMatch(region, seed_pid="4460427", api_key=api_key)
         else:
             match_id = increaseId(last_match)
         region = "kr"
