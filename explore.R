280ed24ac3a275f13722f6524cd2cf620145dadf
Kyle Chung <kylechun9@gmail.com>
2015-11-18 02:07:08 +0800

    update explore draft

--- explore.R
+++ explore.R
@@ -32,14 +32,50 @@ config <- readIniConfig("conf.ini")
 connect(es_base=config$database["elasticsearch_host"],
         es_port=config$database["elasticsearch_port"])
 
-query <- "queueType:NORMAL_5x5_BLIND"
-res <- Search(index="match", type="details", fields=NULL,
-              default_operator="AND", q=query, 
-              search_type="scan", scroll="1m", size=100) %>% scrollAll
 
-timeinfo <- Search(index="match", type="timeinfo", search_type="scan", scroll="1m", size=100) %>% 
-            scrollAll %>%
-            lapply(function(x) x$`_source`) %>%
-            rbindlist
-max(table(timeinfo$createTime)) # maximum number of games per second
+#### some test run
+
+## get time info
+timeinfo <- Search(index="match1", type="details", fields=c("insertTime", "matchCreation"),
+                   search_type="scan", scroll="1m", size=1000) %>% 
+            scrollAll %>% {
+                do.call(rbind, lapply(., function(x) unlist(x$fields))) / 1000
+            } %>%
+            as.data.table
+timeinfo %<>% .[, matchCreation:=as.POSIXct(matchCreation, origin="1970-01-01")]
+
+# match-creation traffics per second
+count_by_sec <- timeinfo[, .N, by=createTime] %T>% setkey(., createTime)
+plot(count_by_sec, type='l')
+
+
+## how many distinct players?
+query <- '
+{
+    "_source": [ "participantIdentities" ],
+    "query": {
+        "filtered": {
+            "query": {
+                "match_all": {}
+            },
+            "filter": {
+                "term" : { "queueType": "RANKED_SOLO_5x5" }
+            }
+        }
+    }
+}'
+
+system.time( # 100 records need 15 sec on pi
+players <- Search("match", "details", body=query, size=100)
+)
+pp <- lapply(players$hits$hits, function(x) x$`_source`$participantIdentities) %>%
+{
+    lapply(., function(x) 
+           sapply(x, function(y) y$player$summonerId))
+}
+
+system.time( # scroll over 45,000 docs: 1411 sec 
+players <-  Search(index="match", type="details", body=query,
+                   search_type="scan", scroll="1m", size=1000) %>% scrollAll
+)
 
