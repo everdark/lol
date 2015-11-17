2441aa94cbee1408655c9723eedaa8d26c2b740b
Kyle Chung <kylechun9@gmail.com>
2015-11-17 12:01:32 +0800

    use config for index name and doc type

--- match_crawlerd_es.py
+++ match_crawlerd_es.py
@@ -50,20 +50,22 @@ def increaseId(match_id, inc=1):
 
 def main():
     config = ConfigParser.ConfigParser()
-    if len(config.read(['conf.ini'])):
+    if len(config.read(["conf.ini"])):
         api_key = config.get("user", "api_key")
         region = config.get("user", "region")
         log_path = config.get("logging", "log_path")
         logger = getLogger(log_path, level=logging.INFO)
         es_host = config.get("database", "elasticsearch_host")
         es_port = config.get("database", "elasticsearch_port")
-        es = elasticsearch.Elasticsearch(hosts=[{"host": es_host, "port": es_port}])
+        es_index = config.get("database", "elasticsearch_index")
+        es_doctype = config.get("database", "elasticsearch_doctype")
 
-        if not es.indices.exists("match"):
-            dbtools.initElasticIndices(es)
+        es = elasticsearch.Elasticsearch(hosts=[{"host": es_host, "port": es_port}])
+        if not es.indices.exists(es_index):
+            dbtools.initElasticIndices(es, index_name=es_index, doc_type=es_doctype)
             match_id = getLastMatch(region, seed_pid="22071749", api_key=api_key)
         else:
-            last_updated = es.search("match", "brief", size=1, sort="insertTime:desc")["hits"]["hits"]
+            last_updated = es.search(es_index, es_doctype, _source=False, size=1, sort="insertTime:desc")["hits"]["hits"]
             if not len(last_updated):
                 match_id = getLastMatch(region, seed_pid="22071749", api_key=api_key)
             else:
@@ -71,12 +73,11 @@ def main():
                 match_id = increaseId(last_match)
 
         while True:
-            # crawl the match data
             status_code, match_details = getMatchDetails(region=region, match_id=match_id, api_key=api_key)
             logger.info("crawled possible matchId %s | status code resolved: %s" % (match_id, status_code))
             if match_details is not None:
                 mid = match_details.pop("matchId") # replace the default auto-index field _id
-                es.create("match", "details", id=mid, body=match_details)
+                es.create(es_index, es_doctype, id=mid, body=match_details)
             match_id = increaseId(match_id)    
             time.sleep(1) # to avoid api overshooting (max 500 queries per 10 min)
     else:
@@ -84,7 +85,7 @@ def main():
         exit(1)
 
 def runAsDaemon():
-    with daemon.DaemonContext(working_directory="."):
+    with daemon.DaemonContext(working_directory='.'):
         try:
             main()
         except:
