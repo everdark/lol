ba6542a4ac1616f6400976bb14208345054c520e
Kyle Chung <kylechun9@gmail.com>
2015-11-24 18:33:05 +0800

    add cralwer func

--- crawlertool.py
+++ crawlertool.py
@@ -1,4 +1,8 @@
 
+import requests
+import json
+
+
 def getLastMatch(region, seed_pid, api_key, ver="v1.3"):
     api_server = "https://%s.api.pvp.net" % region
     api_call = "%s/api/lol/%s/%s/game/by-summoner/%s/recent?" % (api_server, region, ver, seed_pid)
@@ -22,3 +26,20 @@ def getMatchDetails(region, match_id, api_key, ver="v2.2"):
         return r.status_code, rj
     else:
         return r.status_code, None
+
+def getAllChampionInfo(api_key, region="na", ver="v1.2", outfile="champion_id.json"):
+    api_server = "https://global.api.pvp.net" 
+    api_call = "%s/api/lol/static-data/%s/%s/champion" % (api_server, region, ver)
+    qs = {"api_key": api_key}
+    r = requests.get(api_call, params=qs)
+    if r.status_code == 200:
+        rj = r.json()
+        with open(outfile, 'w') as f:
+            for k, v in rj["data"].items():
+                f.write(json.dumps(v) + "\n")
+    else:
+        return None
+
+
+
+
