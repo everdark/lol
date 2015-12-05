d402fa89b701ba9f865edb02a686090ed7f6f1a6
kylechung <kylechun9@gmail.com>
2015-12-06 00:41:38 +0800

    write out file outside function

--- dump_champion_info.py
+++ dump_champion_info.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import json
 import ConfigParser
 
-import crawlertool
+import crawlertools
 
 
 def main():
@@ -10,7 +11,11 @@ def main():
     if len(config.read(["conf.ini"])):
         api_key = config.get("user", "api_key")
         outfile = config.get("static", "champion_id")
-        crawlertool.getAllChampionInfo(api_key, region="na", ver="v1.2", outfile=outfile)
+        res = crawlertools.getAllChampionInfo(api_key, region="na", ver="v1.2")
+        if res is not None:
+            with open(outfile, 'w') as f:
+                for k, v in res["data"].items():
+                    f.write(json.dumps(v) + "\n")
     else:
         print "File conf.ini not found. Program aborted."
         exit(1)
